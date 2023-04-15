eb2e38e292c3b14ad14d5fb418c4145b1a1adee4
lawrence <lawrenceanthony70@gmail.com>
2023-04-15 23:07:10 +0100

    added default font and color styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
-import '@/styles/globals.css'
+import { GlobalStyles } from "@mui/material";
+import { MuiGlobalStyle } from "@/styles/muiGlobalStyle";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { customTheme } from "@/styles/muiGlobalStyle";
+
+const theme = createTheme(customTheme);
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles styles={MuiGlobalStyle} />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
---
+++ styles/muiGlobalStyle.js
@@ -0,0 +1,112 @@
+export const MuiGlobalStyle = {
+  "*": {
+    margin: 0,
+    padding: 0,
+    boxSizing: "border-box",
+    fontFamily: "Ubuntu, sans-serif",
+  },
+};
+
+export const customTheme = {
+  palette: {
+    primary: {
+      main: "#9138E8",
+    },
+
+    customGreen: "#2D9350",
+
+    accent: "#FCAE39",
+
+    accentPink: "#E31276",
+
+    customRed: "#E31276",
+
+    whiteBg: "#FFFFFF",
+
+    altBg: "#F8F9FA",
+
+    violetBg: "#EFE9F5",
+  },
+
+  //decleration of different font styles.
+
+  typography: {
+    fontFamily: "Ubuntu, sans-serif",
+
+    headerMain: {
+      fontSize: 57,
+      fontWeight: 400,
+    },
+
+    headerMainBold: {
+      fontSize: 57,
+      fontWeight: "bold",
+    },
+
+    headlineMain: {
+      fontSize: 32,
+      fontWeight: 400,
+    },
+
+    headlineMainBold: {
+      fontSize: 32,
+      fontWeight: "bold",
+    },
+
+    title: {
+      fontSize: 22,
+      fontWeight: 400,
+    },
+
+    titleBold: {
+      fontSize: 22,
+      fontWeight: "bold",
+    },
+
+    body: {
+      fontSize: 16,
+      fontWeight: 400,
+    },
+
+    bodyBold: {
+      fontSize: 16,
+      fontWeight: "bold",
+    },
+
+    label: {
+      fontSize: 14,
+      fontWeight: 400,
+    },
+
+    labelBold: {
+      fontSize: 14,
+      fontWeight: "bold",
+    },
+
+    caption: {
+      fontSize: 12,
+      fontWeight: 400,
+    },
+  },
+
+  components: {
+    MuiTypography: {
+      defaultProps: {
+        variantMapping: {
+          // Map the new variant to render a semantic html tags
+          headerMain: "h1",
+          headerMainBold: "h1",
+          headlineMain: "h2",
+          headlineMainBold: "h2",
+          title: "h4",
+          titleBold: "h4",
+          body: "p",
+          bodyBold: "p",
+          label: "h5",
+          labelBold: "h5",
+          caption: "h6",
+        },
+      },
+    },
+  },
+};
