8a1b21e04c9bce2a51c8c662bf7e909ea02df8ac
lawrence <lawrenceanthony70@gmail.com>
2023-05-22 14:41:34 +0100

    trying to reslove and merge

--- components/navigation/NavComp.jsx
+++ components/navigation/NavComp.jsx
@@ -21,7 +21,9 @@ const NavComp = () => {
 
           <NavLinkContainer>
             <NavLinkWhiteText href="/user/login">Login</NavLinkWhiteText>
-            <NavLinkWhiteButton href="/user/register">Sign up</NavLinkWhiteButton>
+            <NavLinkWhiteButton href="/user/register">
+              Sign up
+            </NavLinkWhiteButton>
           </NavLinkContainer>
         </InnerNav>
       </Nav>
