18bdd41104050d39b21d5a9f8cf31d26d97ac046
Maxwell D. Dorliea <deboy170@gmail.com>
2023-06-09 12:12:13 +0000

    Fix: Updated the contact us page ui to use the new design

--- components/clientUi/contact/ContactInfo.jsx
+++ components/clientUi/contact/ContactInfo.jsx
@@ -1,4 +1,4 @@
-import { Typography, Stack } from '@mui/material';
+import { Typography, Stack, Paper, Grid } from '@mui/material';
 import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
 import LocalPhoneOutlinedIcon from '@mui/icons-material/LocalPhoneOutlined';
 import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
@@ -7,34 +7,64 @@ import TwitterIcon from "@mui/icons-material/Twitter";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import YouTubeIcon from "@mui/icons-material/YouTube";
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import Image from 'next/image';
 
 const ContactInfo = () => {
   return (
-    <Stack spacing={4}>
-      <Typography variant='h2' color='primary'>Contact Us</Typography>
-      <Stack spacing={2}>
-        <Stack spacing={2} direction='row'>
-          <EmailOutlinedIcon color='primary' />
-          <Typography variant='body'>info@ngoane.com</Typography>
-        </Stack>
-        <Stack spacing={2} direction='row'>
-          <LocalPhoneOutlinedIcon color='primary' />
-          <Typography variant='body'>+233 (0) 24 000 0000</Typography>
-        </Stack>
-        <Stack spacing={2} direction='row'>
-          <LocationOnOutlinedIcon color='primary' />
-          <Typography variant='body'>East Legon Hill, Accre - Ghana</Typography>
-        </Stack>
+    <Paper elevation={24} sx={{ padding: "5% 10%", marginRight: "10%"}}>
+      <Stack spacing={5}>
+        <Typography variant='headlineMainBold' color='primary'>Contact Information</Typography>
+        <Typography variant='body' sx={{ color: "rgb(112, 109, 112)"}}>You can reach us through the following contact information</Typography>
+        <Paper elevation={24} sx={{ padding: "2%"}}>
+          <Grid container>
+            <Grid item xs={4}>
+              <Image
+                src='/contact/phone.svg'
+                height={64}
+                width={64}
+                alt='Phone icon'
+              />
+            </Grid>
+            <Grid item xs={8}>
+              <Typography variant='titleBold'>Phone</Typography>
+              <Typography variant='body'>+233 (0) 24 000 0000</Typography>
+            </Grid>
+          </Grid>
+        </Paper>
+        <Paper elevation={24} sx={{ padding: "2%"}}>
+          <Grid container>
+            <Grid item xs={4}>
+              <Image
+                src='/contact/location.svg'
+                height={64}
+                width={64}
+                alt='Location icon'
+              />
+            </Grid>
+            <Grid item xs={8}>
+              <Typography variant='titleBold'>Location</Typography>
+              <Typography variant='body'>East Legon Hill, Accre - Ghana</Typography>
+            </Grid>
+          </Grid>
+        </Paper>
+        <Paper elevation={24} sx={{ padding: "2%"}}>
+          <Grid container>
+            <Grid item xs={4}>
+              <Image
+                src='/contact/mail.svg'
+                height={64}
+                width={64}
+                alt='Mail icon'
+              />
+            </Grid>
+            <Grid item xs={8}>
+              <Typography variant='titleBold'>Email</Typography>
+               <Typography variant='body'>info@ngoane.com</Typography>
+            </Grid>
+          </Grid>
+        </Paper>
       </Stack>
-      <Typography variant='bodyBold'>Follow Us On</Typography>
-      <Stack spacing={2} direction='row'>
-        <FacebookIcon color="primary" />
-        <TwitterIcon color="primary" />
-        <InstagramIcon color="primary" />
-        <LinkedInIcon color="primary" />
-        <YouTubeIcon color="primary" />
-      </Stack>
-    </Stack>
+    </Paper>
   )
 }
 
--- components/clientUi/contact/StyledContact.js
+++ components/clientUi/contact/StyledContact.js
@@ -7,6 +7,21 @@ export const ContactContainer = styled(Stack)`
   justify-content: space-around;
   align-items: center;
   margin-top: 10rem;
+  `
+
+  export const StyledSection = styled.section`
+  width: 100%;
+  background-image: url('/contact/hero.svg'), None;
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-color: rgba(146, 56, 232, 0.4);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding-top: 12%;
+  padding-bottom: 12%;
 `
 
 export const MessageButton = styled(CustomButton)`
